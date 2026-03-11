3f2759b05a1fc1b2a5e168d07230f0fab2937710
Jha <2478605@cognizant.com>
2026-03-11 12:15:56 +0530

    enums and settlemetrecode added

--- SwiftPay/SwiftPay/Config/Configuration/ReconciliationRecordConfiguration.cs
+++ SwiftPay/SwiftPay/Config/Configuration/ReconciliationRecordConfiguration.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using YourNamespace.Models;
+
 
 namespace SwiftPay.Config.Configuration
 {
--- SwiftPay/SwiftPay/Config/Configuration/SettlementReconciliationConfiguraton.cs
+++ SwiftPay/SwiftPay/Config/Configuration/SettlementReconciliationConfiguraton.cs
@@ -4,39 +4,52 @@ using SwiftPay.Models;
 
 namespace SwiftPay.Config.Configuration
 {
-	public class SettlementBatchConfiguration : IEntityTypeConfiguration<SettlementBatch>
-	{
-		public void Configure(EntityTypeBuilder<SettlementBatch> builder)
-		{
-			builder.HasKey(s => s.BatchID);
+    // ReconciliationRecord configuration
+    public class ReconciliationRecordConfiguration : IEntityTypeConfiguration<ReconciliationRecord>
+    {
+        public void Configure(EntityTypeBuilder<ReconciliationRecord> builder)
+        {
+            builder.HasKey(r => r.ReconID);
+            builder.Property(r => r.ReferenceType).IsRequired().HasMaxLength(50);
+            builder.Property(r => r.ReferenceID).IsRequired().HasMaxLength(100);
+            builder.Property(r => r.ExpectedAmount).IsRequired().HasPrecision(18, 4);
+            builder.Property(r => r.ActualAmount).IsRequired().HasPrecision(18, 4);
+            builder.Property(r => r.Result).IsRequired().HasMaxLength(50);
+            builder.Property(r => r.ReconDate).IsRequired().HasDefaultValueSql("GETDATE()");
+        }
+    }
 
-			builder.Property(s => s.Corridor)
-				   .IsRequired()
-				   .HasMaxLength(10);
+    // SettlementBatch configuration
+    public class SettlementBatchConfiguration : IEntityTypeConfiguration<SettlementBatch>
+    {
+        public void Configure(EntityTypeBuilder<SettlementBatch> builder)
+        {
+            builder.HasKey(s => s.BatchID);
 
-			builder.Property(s => s.PeriodStart)
-				   .IsRequired();
+            builder.Property(s => s.Corridor)
+                   .IsRequired()
+                   .HasMaxLength(10);
 
-			builder.Property(s => s.PeriodEnd)
-				   .IsRequired();
+            builder.Property(s => s.PeriodStart).IsRequired();
+            builder.Property(s => s.PeriodEnd).IsRequired();
 
-			builder.Property(s => s.ItemCount)
-				   .HasDefaultValue(0);
+            builder.Property(s => s.ItemCount).HasDefaultValue(0);
 
-			builder.Property(s => s.TotalSendAmount)
-				   .HasPrecision(15, 4)
-				   .HasDefaultValue(0.0000m);
+            builder.Property(s => s.TotalSendAmount)
+                   .HasPrecision(15, 4)
+                   .HasDefaultValue(0.0000m);
 
-			builder.Property(s => s.TotalPayoutAmount)
-				   .HasPrecision(15, 4)
-				   .HasDefaultValue(0.0000m);
+            builder.Property(s => s.TotalPayoutAmount)
+                   .HasPrecision(15, 4)
+                   .HasDefaultValue(0.0000m);
 
-			builder.Property(s => s.CreatedDate)
-				   .HasDefaultValueSql("GETDATE()");
+            builder.Property(s => s.CreatedDate)
+                   .HasDefaultValueSql("GETDATE()");
 
-			builder.Property(s => s.Status)
-				   .HasMaxLength(50)
-				   .HasDefaultValue("Open");
-		}
-	}
+            builder.Property(s => s.Status)
+                   .HasConversion<string>() // store enum as string
+                   .HasMaxLength(50)
+                   .HasDefaultValue("Open");
+        }
+    }
 }
