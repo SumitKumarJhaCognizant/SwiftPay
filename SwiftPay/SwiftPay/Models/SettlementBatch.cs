3866cca7fb552a5b531d0248c13a587792a95075
Jha <2478605@cognizant.com>
2026-03-11 09:57:13 +0530

    Settlement Batch model and configuration files added

---
+++ SwiftPay/SwiftPay/Models/SettlementBatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace SwiftPay.Models
+{
+	public class SettlementBatch
+	{
+		[Key]
+		public int BatchID { get; set; }
+		
+		public string Corridor { get; set; }
+		public DateTime PeriodStart { get; set; }
+		public DateTime PeriodEnd { get; set; }
+		public int ItemCount { get; set; }
+		public decimal TotalSendAmount { get; set; }
+		public decimal TotalPayoutAmount { get; set; }
+		public DateTime CreatedDate { get; set; }
+		public string Status { get; set; }
+
+
+
+	}
+}
